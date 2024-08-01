953a03926a84a236de7bae2db61838fa0f3fe9a2
chlience <chlience@outlook.com>
2024-08-01 17:07:54 +0800

    Update index.js and wrangler.toml to my website

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.chlience.com": dockerHub,
+  "quay.chlience.com": "https://quay.io",
+  "gcr.chlience.com": "https://gcr.io",
+  "k8s-gcr.chlience.com": "https://k8s.gcr.io",
+  "k8s.chlience.com": "https://registry.k8s.io",
+  "ghcr.chlience.com": "https://ghcr.io",
+  "cloudsmith.chlience.com": "https://docker.cloudsmith.io",
+  "ecr.chlience.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.chlience.com": dockerHub,
 };
 
 function routeByHosts(host) {
